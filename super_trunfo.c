073f0a89206b807e0dd02380442a705a9b949da3
JnhSoares <jnhsoares1@gmail.com>
2025-02-18 00:47:46 +0000

    Criei um printf

--- super_trunfo.c
+++ super_trunfo.c
@@ -13,37 +13,35 @@ int número;
 
 printf ("Digite o estado: \n");
 scanf ("%s", &estado);
-
+    
 printf ("Digite o código da carta: \n");
 scanf ("%s", &código);
-
+    
 printf ("Digite o nome da cidade: \n");
 scanf ("%s", &nome);
-
+    
 printf ("Digite a população: \n");
-scanf ("%d", &população);
-
+scanf ("%i", &população);
+    
 printf ("Digite a área em km²: \n");
 scanf ("%f", &área);
-
+    
 printf ("Digite o PIB: \n");
 scanf ("%f", &PIB);
-
-printf ("Digite o número de pontos turísticos \n");
+    
+printf ("Digite o número de pontos turísticos: \n");
 scanf ("%d", &número);
-
-
+    
+printf ("Carta 1:\n");
 printf ("Estado: %s \n", estado);
 printf ("Código: %s \n", código);
 printf ("Nome da cidade: %s \n", nome);
 printf ("População: %d \n", população);
 printf ("Área: %f km²\n", área);
-printf ("PIB: %f bilhões de reais \n", PIB);
+printf ("PIB: %.f bilhões de reais \n", PIB);
 printf ("Número de pontos turísticos: %d \n", número);
-
-
+    
+    
 return 0;
 
-
-    
 }
